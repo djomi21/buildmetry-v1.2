9c47dba5b5fa501c61d69f87d189ccdd0c6acecd
djomi21 <ocpflorida@gmail.com>
2026-04-02 15:13:17 -0400

    CO Fix

---
+++ frontend/src/components/ChangeOrders.jsx
@@ -0,0 +1,10 @@
+const ChangeOrders = () => {
+  return (
+    <div>
+      <h1>Change Orders</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default ChangeOrders;
